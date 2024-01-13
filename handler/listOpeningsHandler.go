c57c1c0bbdfa72fe8110b8f3d0fc78f68d82d982
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-12 21:16:10 -0300

    feat: created list opeings handler

--- handler/listOpeningsHandler.go
+++ handler/listOpeningsHandler.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/guivictorr/gopportunities/schemas"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "error trying to list openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
 }
