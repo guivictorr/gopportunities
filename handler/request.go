ec742a4d1e1790c326abbdd16fbff02be7f972b1
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-12 21:35:12 -0300

    feat: update opening handler

--- handler/request.go
+++ handler/request.go
@@ -40,3 +40,20 @@ func (r *CreateOpeningRequest) Validate() error {
 
 	return nil
 }
+
+type UpdateOpeningRequest struct {
+	Remote   *bool  `json:"remote"`
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Salary > 0 || r.Remote != nil {
+		return nil
+	}
+
+	return fmt.Errorf("at least one valid field must be provided")
+}
