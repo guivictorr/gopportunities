7744e51a3886601582b986a3fce35cd78dd435f3
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-01-13 20:56:03 -0300

    docs: created showOpeningHandler docs

--- handler/showOpeningHandler.go
+++ handler/showOpeningHandler.go
@@ -8,6 +8,18 @@ import (
 	"github.com/guivictorr/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Show Opening
+// @Description Show a job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} ShowOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [get]
 func ShowOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
